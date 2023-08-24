0fa75003efd2fe0c0d39700a89d03476cab36036
Tyler Twining <twijn@twijn.net>
2023-08-24 02:37:35 -0400

    Handle bans & timeouts, delete messages in Discord

---
+++ twitch/listeners/banHandler.js
@@ -0,0 +1,30 @@
+const api = require("../../api/");
+const con = require("../../database");
+
+const listener = {
+    name: "banHandler",
+    eventType: "on",
+    event: "ban",
+    execute: async (channel, username, reason, tags) => {
+        con.query("select discord_id, bridge_id from bridge__chat where user_id = ? and deleted = false;", [
+            tags["target-user-id"],
+        ], (err, res) => {
+            if (err) return api.Logger.warning(err);
+
+            for (let i = 0; i < res.length; i++) {
+                const row = res[i];
+                con.query("update bridge__chat set deleted = true where user_id = ? and bridge_id = ?;", [
+                    tags["target-user-id"],
+                    row.bridge_id,
+                ], err => {
+                    if (err) api.Logger.warning(err);
+                });
+
+                const bridge = api.Bridge.getBridgeById(row.bridge_id);
+                bridge.channel.messages.delete(row.discord_id).catch(api.Logger.warning);
+            }
+        });
+    }
+};
+
+module.exports = listener;
